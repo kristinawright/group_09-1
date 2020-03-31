b437920cca1403adf092c0a14b1fdea91a2e4672
kristinawright <k-wright07@hotmail.com>
2020-03-31 14:13:45 -0700

    added violin plot 2 and callbacks

--- app.R
+++ app.R
@@ -23,8 +23,28 @@ load_clean <- function(path_clean) {
            col_types=cols())
 }
 path_clean <- "data/clean_listings.csv"
+
+mean_price <- function(df){
+  df %>% 
+    # calculate the mean price for each district for plot ordering
+    group_by(district) %>%
+    summarize(mean = mean(price)) %>%
+    arrange(desc(mean)) # mean price in descending order
+}
+mean.price <- mean_price(clean.dat)
+
+mean_price2 <- function(df){
+  df %>% 
+    # calculate the mean price for each district for plot ordering
+    group_by(room_type) %>%
+    summarize(mean = mean(price)) %>%
+    arrange(desc(mean)) # mean price in descending order
+}
+mean.price2 <- mean_price2(clean.dat)
+
 clean.dat <- load_clean(path_clean) %>%
-  mutate(district = factor(district, levels = unique(mean.price$district)))  #factor district by descending mean price
+  mutate(district = factor(district, levels = unique(mean.price$district))) %>% #factor district by descending mean price
+  mutate(room_type = factor(room_type, levels = unique(mean.price2$room_type))) #factor room_type by descending mean price
 
 
 # Create the slider
@@ -45,7 +65,7 @@ logbutton <- dccRadioItems(
   value = 'linear'
 )
 
-# Create the checklist
+# 1: Create the checklist
 xaxisKey <- tibble(labels = as.character(levels(clean.dat$district)),
                    value = as.character(levels(clean.dat$district)))
 
@@ -58,24 +78,28 @@ checklist<- dccChecklist(
   value=as.character(levels(clean.dat$district))
 )
 
+# 2: Create the checklist
+xaxisKey2 <- tibble(labels = as.character(levels(clean.dat$room_type)),
+                   value = as.character(levels(clean.dat$room_type)))
+
+checklist2<- dccChecklist(
+  id = "checklist2",
+  options = map(
+    1:nrow(xaxisKey2), function(i){
+      list(label = xaxisKey2$labels[i], value = xaxisKey2$value[i])
+    }),
+  value=as.character(levels(clean.dat$room_type))
+)
+
 
 # 1: violin plot
-
-mean_price <- function(df){
-  df %>% 
-    # calculate the mean price for each district for plot ordering
-    group_by(district) %>%
-    summarize(mean = mean(price)) %>%
-    arrange(desc(mean)) # mean price in descending order
-}
-mean.price <- mean_price(clean.dat)
 
 violin_plot1 <- function(price.slider = c(0, max.price), scale = "linear", districtc = as.character(levels(clean.dat$district))){
   
   df1<-clean.dat
   df1<- clean.dat %>%
     filter(clean.dat$price > price.slider[1]) %>%
-    filter(clean.dat$price < price.slider[2]) %>%
+    filter(price < price.slider[2]) %>%
     filter(district %in% districtc) 
     
   
@@ -96,11 +120,40 @@ violin_plot1 <- function(price.slider = c(0, max.price), scale = "linear", distr
   }
 
 
+# 2: violin plot
+
+violin_plot2 <- function(price.slider = c(0, max.price), scale = "linear", room.typec = as.character(levels(clean.dat$room_type))){
+
+  df2<-clean.dat
+  df2<- clean.dat %>%
+    filter(clean.dat$price > price.slider[1]) %>%
+    filter(price < price.slider[2]) %>%
+    filter(room_type %in% room.typec)
+
+
+  p2<-df2%>%
+    ggplot(aes(room_type, price)) +
+    geom_violin(stat = "ydensity") +
+    ylab(paste("Price (", "\u20AC", ")", sep='')) +
+    xlab("Room Type") +
+    ggtitle(paste0("Distribution of Price from ", price.slider[1], " to ", price.slider[2], " \u20AC by Room Type over time (Scale : ", scale,")")) +
+    theme_bw(15) +
+    theme(plot.title = element_text(size = 14), axis.text.x = element_text(angle = 60, hjust = 1))
+
+  if (scale == 'log'){
+    p2 <- p2 + scale_y_continuous(trans='log10')
+  }
+
+  ggplotly(p2)
+}
+
+
 
 # Assign components to variables
 heading_main = htmlH1('My Dash app :)')
 
 graph_1 = dccGraph(id='violin1',figure = violin_plot1())
+graph_2 = dccGraph(id='violin2',figure = violin_plot2())
 
 
 app <- Dash$new()
@@ -118,7 +171,9 @@ app$layout(
 			 htmlLabel('Select y scale : '),
 			 logbutton,
 			 checklist,
-			 graph_1
+			 checklist2,
+			 graph_1,
+			 graph_2
 		)
 	)
 )
@@ -134,6 +189,18 @@ app$callback(
   #this translates your list of params into function arguments
   function(price.sliderr, yaxis_scale, checking) {
     violin_plot1(price.sliderr, yaxis_scale, checking)
+  })
+
+app$callback(
+  #update figure of gap-graph
+  output=list(id = 'violin2', property='figure'),
+  #based on values of components
+  params=list(input(id = 'slider', property='value'),
+              input(id = 'yaxis-type', property='value'),
+              input(id = 'checklist2', property='value')),
+  #this translates your list of params into function arguments
+  function(price.sliderr, yaxis_scale, checking2) {
+    violin_plot2(price.sliderr, yaxis_scale, checking2)
   })
 
 app$callback(
